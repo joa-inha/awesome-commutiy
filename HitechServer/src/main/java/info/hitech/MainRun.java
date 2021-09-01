047dbed781a02950c6da5bcba4f308839cb2ce66
Zzu-h <juho7785@naver.com>
2021-09-01 19:12:17 +0900

    Update

--- HitechServer/src/main/java/info/hitech/MainRun.java
+++ HitechServer/src/main/java/info/hitech/MainRun.java
@@ -1,12 +1,10 @@
 package info.hitech;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
+@SpringBootApplication //(exclude = WebMvcAutoConfiguration.class)
 public class MainRun {
     public static void main(String[] args) {
         SpringApplication.run(MainRun.class, args);
