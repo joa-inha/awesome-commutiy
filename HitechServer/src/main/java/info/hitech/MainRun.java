7af41a100c081119b0dc42d35a98151e229195ca
Zzu-h <juho7785@naver.com>
2021-09-02 17:26:08 +0900

    Update: clean code

--- HitechServer/src/main/java/info/hitech/MainRun.java
+++ HitechServer/src/main/java/info/hitech/MainRun.java
@@ -2,9 +2,9 @@ package info.hitech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
-@SpringBootApplication //(exclude = WebMvcAutoConfiguration.class)
+@SpringBootApplication(exclude = WebMvcAutoConfiguration.class)
 public class MainRun {
     public static void main(String[] args) {
         SpringApplication.run(MainRun.class, args);
